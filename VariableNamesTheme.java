0b53e8c32cafa8cac0fc15ddb6493fe41722cbc1
binjof <binjof@gmail.com>
2024-03-08 12:22:55 +0300

    Инициация проекта
    - добавил файл с описание проекта
    - добавил реализацию первого класса

---
+++ VariableNamesTheme.java
@@ -0,0 +1,184 @@
+public class VariableNamesTheme {
+    public static void main(String[] args) {
+        // цифра
+        int number = 10;
+        System.out.println("Цифра " + number);
+
+        // сумма чисел
+        int sumOfNumbers = 20;
+        System.out.println("Сумма чисел " + sumOfNumbers);
+
+        // произведение чисел
+        int multiplyingNumbers = 30;
+        System.out.println("Произведение чисел " + multiplyingNumbers);
+
+        // максимальное число
+        int maxNumber = 100;
+        System.out.println("Максимальное число " + maxNumber);
+
+        // количество десятков 
+        int amountOfTens = 50;
+        System.out.println("Количество десятков " + amountOfTens);
+
+        // вес собаки
+        int weightOfDog = 40;
+        System.out.println("Вес собаки " + weightOfDog);
+
+        // исходное число
+        int originalNumber = 50;
+        System.out.println("Исходное число " + originalNumber);
+
+        // процент по вкладу
+        int interestOnDeposit = 13;
+        System.out.println("Процент по вкладу " + interestOnDeposit);
+
+        // символ
+        char symbol = '&';
+        System.out.println("Символ " + symbol);
+
+        // код ошибки
+        int errorCode = 101;
+        System.out.println("Код ошибки " + errorCode);
+
+        // тип сообщения
+        String notification = "Уведомление";
+        System.out.println("Тип сообщения " + notification);
+
+        // число нулей
+        int numberOfZeros = 4;
+        System.out.println("Число нулей " + numberOfZeros);
+
+        // уникальное число
+        int uniqueNumber = 33;
+        System.out.println("Уникальное число " + uniqueNumber);
+
+        // случайное число
+        int randomNumber = 44;
+        System.out.println("Случайное число " + randomNumber);
+
+        // математическое выражение
+        String mathematicalExpression = "a + b = c";
+        System.out.println("Математическое выражение " + mathematicalExpression);
+
+        // выбор
+        int numberSelection = 10;
+        System.out.println("Выбор числа " + numberSelection);
+
+        // счет
+        String gameScore = "4:2";
+        System.out.println("Счет " + gameScore);
+
+        // макимальная длина
+        int maxLineLength = 255;
+        System.out.println("Максимальная длина " + maxLineLength);
+
+        // пункт меню
+        String menuItem = "Оплата";
+        System.out.println("Пункт меню " + menuItem);
+
+        // стоимость кофе на вынос
+        int costOfCoffeToGo = 65;
+        System.out.println("Стоимость кофе на вынос " + costOfCoffeToGo);
+
+        // дата начала
+        String startDate = "26.02.24";
+        System.out.println("Дата начала " + startDate);
+
+        // окончание диапазона
+        String endOfRange = "k9999";
+        System.out.println("Окончание диапазона " + endOfRange);
+
+        // полное имя работника месяца
+        String fullNameofEmployeeOfTheMonth = "Иванов Иван Иванович";
+        System.out.println("Полное имя работника месяца " + fullNameofEmployeeOfTheMonth);
+
+        // заголовок электронной книги
+        String ebookTitle = "Глава 1";
+        System.out.println("Заголовок электронной книги " + ebookTitle);
+
+        // размер 
+        String size = "L";
+        System.out.println("Размер " + size);
+
+        // вместимость
+        String capacity = "1024 мб";
+        System.out.println("Вместимость " + capacity);
+
+        // счетчик
+        int meter = 21343;
+        System.out.println("Счетчик " + meter);
+
+        // путь до файла
+        String pathToFile = "D:/Java/StartJava";
+        System.out.println("Путь до файла " + pathToFile);
+
+        // количество чисел в каждой строке
+        int amountOfNumbersInEveryLine = 30;
+        System.out.println("Количество чисел в каждой строке " + amountOfNumbersInEveryLine);
+
+        // сотни равны?
+        boolean hundreadsEqual = true;
+        System.out.println("Сотни равны? " + hundreadsEqual);
+
+        // компьютер включен?
+        boolean computerTurnedOn = false;
+        System.out.println("Компьютер включен? " + computerTurnedOn);
+
+        // есть равные цифры?
+        boolean equalNumbers = false;
+        System.out.println("Есть равные цифры? " + equalNumbers);
+
+        // (что-либо) создано?
+        boolean created = true;
+        System.out.println("Создано? " + created);
+
+        // (что-либо) пустое?
+        boolean blank = true;
+        System.out.println("Пустое? " + blank);
+
+        // (что-либо) активное?
+        boolean active = false;
+        System.out.println("Активное " + active);
+
+        // (что-либо) новое?
+        boolean resent = true;
+        System.out.println("Новое? " + resent);
+
+        // электронная почта действительная?
+        boolean emailValid = false;
+        System.out.println("Электронная почта действительная? " + emailValid);
+
+        // имеются уникальные строки?
+        boolean uniqueStrings = true;
+        System.out.println("Имеются электронные строки? " + uniqueStrings);
+
+        // старый universally unique identifier
+        String uuid = "UUID";
+        System.out.println("Старый universally unique identifier " + uuid);
+
+        // производительность оперативной памяти
+        String ram = "Samsung";
+        System.out.println("Производительность оперативной памяти " + ram);
+
+        // емкость жесткого диска
+        String hdd = "1ТБ";
+        System.out.println("Емкость жесткого диска " + hdd);
+
+        // протокол передачи гипертекста
+        String http = "Hypertext Transfer Protocol";
+        System.out.println("Протокол передачи гипертекста" + http);
+
+        // сокращенный uniform resource locator
+        String url = "URL";
+        System.out.println("Cокращенный uniform resource locator " + url);
+
+        // новый индентификатор клиента
+        String cuid = "CUID";
+        System.out.println("Новый идентификатор клиента " + cuid);
+
+        // кодировка American Standart Code for information interchange
+        String ascii = "ASCII";
+        System.out.println("Кодировка American Standart Code for information interchange " + ascii);
+    }
+}
+
